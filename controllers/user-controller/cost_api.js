4889788917a54ff0b64ec4cd0686ed4be867dbd4
nightblood <nightblood@localhost.localdomain>
2020-12-25 13:05:10 +0100

    price

--- controllers/user-controller/cost_api.js
+++ controllers/user-controller/cost_api.js
@@ -3,8 +3,8 @@ module.exports = async(req,res) => {
         const{distance, time, requestType, size, economy_choice} = req.body
         
         if(requestType === "Bike"){
-            let distances = distance * 6;
-            let timeAmt = time * 5;
+            let distances = distance * 9;
+            let timeAmt = time * 20;
             let sumFare= distances + timeAmt
 
             return res.status(200).send({
@@ -13,42 +13,10 @@ module.exports = async(req,res) => {
                 payload: sumFare
             })
         }
-        else if(requestType === "Delivery" && size==="Small"){
-            let distances = distance * 2;
-            let timeAmt = time * 2;
-            let sumFare= distances + timeAmt + 200
 
-            return res.status(200).send({
-                status: "OK",
-                message: "Total cost",
-                payload: sumFare
-            })
-        }
-        else if(requestType === "Delivery" && size==="Medium"){
-            let distances = distance * 2;
-            let timeAmt = time * 2;
-            let sumFare= distances + timeAmt + 300
-
-            return res.status(200).send({
-                status: "OK",
-                message: "Total cost",
-                payload: sumFare
-            })
-        }
-        else if(requestType === "Delivery" && size==="Large"){
-            let distances = distance * 2;
-            let timeAmt = time * 2;
-            let sumFare= distances + timeAmt + 500
-
-            return res.status(200).send({
-                status: "OK",
-                message: "Total cost",
-                payload: sumFare
-            })
-        }
         else if(requestType === "Delivery"){
-            let distances = distance * 2;
-            let timeAmt = time * 2;
+            let distances = distance * 9;
+            let timeAmt = time * 20;
             let sumFare= distances + timeAmt
 
             return res.status(200).send({
@@ -58,8 +26,8 @@ module.exports = async(req,res) => {
             })
         }
         else if(requestType==="Ride" && economy_choice=="standard"){
-            let distances = distance * 34;
-            let timeAmt = time * 30;
+            let distances = distance * 30;
+            let timeAmt = time * 34;
             let sumFare= distances + timeAmt
 
             return res.status(200).send({
@@ -69,8 +37,8 @@ module.exports = async(req,res) => {
             })
         }
         else if(requestType==="Ride" && economy_choice==="comfort2"){
-            let distances = distance * 34;
-            let timeAmt = time * 30;
+            let distances = distance * 30;
+            let timeAmt = time * 34;
             let sumFare= distances + timeAmt 
             let half = Math.floor((sumFare/3)+sumFare)
 
